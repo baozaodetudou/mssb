6cc8c0d5f753ff11f10c98a31366850d32be5ea1
doumao <shenren1993@gmail.com>
2025-11-05 10:16:56 +0800

    fix: 调整 sing-box 监听范围

--- watch/watch_sing_box.sh
+++ watch/watch_sing_box.sh
@@ -4,11 +4,19 @@ sleep 60
 # 需要监听的目录
 CONFIG_DIR="/mssb/sing-box"
 
-# 使用 inotifywait 仅监听 config.json 的变动
-while true; do
-  inotifywait -e modify,create,delete -r "$CONFIG_DIR" --include 'config\.json$'
-  echo "Sing-box 配置文件发生变化，重启 Sing-box 服务..."
-
-  # 通过 supervisorctl 重启 Sing-box 服务
-  supervisorctl restart sing-box && systemctl restart sing-box-router
-done
+# 使用 inotifywait 监听 config.json，忽略指定子目录
+while read -r EVENT FILE; do
+  case "$FILE" in
+    "$CONFIG_DIR"/config.json)
+      echo "Sing-box 配置文件发生变化，重启 Sing-box 服务..."
+      supervisorctl restart sing-box && systemctl restart sing-box-router
+      ;;
+    *)
+      continue
+      ;;
+  esac
+done < <(
+  inotifywait -q -m -e modify,create,delete,move -r "$CONFIG_DIR" \
+    --exclude '/(adguard|gen|genblank|rule|srs|unpack|webinfo)(/|$)' \
+    --format '%e %w%f'
+)
